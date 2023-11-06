246315a302529820d7a20317898ee27e0c6a474b
unknown <manshuchatrath20@gmail.com>
2023-11-06 06:32:49 -0500

    made changes

---
+++ key.js
@@ -0,0 +1,9 @@
+module.exports = {
+  key: {
+    stripe_key:
+      "sk_test_51HvZrNHtX5JzPZUWNl6CVNOkRcM2c2Nrs3KR1sCXwMm0B2wcyTZ1HBoiDqHBByJ1eg26M2FOAyYO73u8MAmVYzHR00OA5YOksn",
+    password: "wixifrdkwtkvgafp",
+    email: "jack.germanshepherd@gmail.com",
+    jwtSecret: "secret",
+  },
+};
